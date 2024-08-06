9710218ad02bc94de4e6e6afac540f37981a0f68
Hunternif <hunternif@gmail.com>
2024-08-06 23:22:51 +0100

    Define game types and firestore converters

---
+++ shared/src/firestore-converters.ts
@@ -0,0 +1,193 @@
+import {
+  FConverter,
+  FDocSnapshot,
+  FTimestamp,
+  fServerTimestamp,
+} from '../firestore-adapter';
+import {
+  defaultLobbySettings,
+  EntryStats,
+  GameEntry,
+  GameLobby,
+  GameTurn,
+  Language,
+  LobbySettings,
+  PlayerInLobby,
+  PlayerResponse,
+  QuizUser,
+  TestGroup,
+  UserStats,
+} from './types';
+import {
+  copyFields,
+  copyFields2,
+  mapToObject,
+  objectToMap,
+  removeUndefined,
+} from './utils';
+
+export const lobbyConverter: FConverter<GameLobby> = {
+  toFirestore: (lobby: GameLobby) => {
+    return copyFields2(
+      lobby,
+      {
+        time_created: lobby.time_created
+          ? FTimestamp.fromDate(lobby.time_created)
+          : fServerTimestamp(), // set new time when creating a new lobby
+        test_groups: Array.from(lobby.test_groups),
+        questions: lobby.questions.map(mapFromEntry),
+      },
+      ['id', 'players', 'turns'],
+    );
+  },
+  fromFirestore: (snapshot: FDocSnapshot) => {
+    const data = snapshot.data();
+    const settings: LobbySettings = data.settings
+      ? mapSettings(data.settings)
+      : defaultLobbySettings();
+    const ret = new GameLobby(
+      snapshot.id,
+      data.creator_uid,
+      settings,
+      data.status,
+    );
+    ret.current_turn_id = data.current_turn_id;
+    ret.time_created = (data.time_created as FTimestamp | null)?.toDate();
+    ret.test_groups = new Set<TestGroup>(data.test_groups ?? []);
+    ret.questions = (data.questions ?? []).map(mapToEntry);
+    ret.next_lobby_id = data.next_lobby_id;
+    return ret;
+  },
+};
+
+function mapSettings(data: any): LobbySettings {
+  return copyFields2(defaultLobbySettings(), removeUndefined(data));
+}
+
+function mapToEntry(data: any): GameEntry {
+  const meanings = new Map<Language, string[]>();
+  // TODO: make this type-safe:
+  // See https://stackoverflow.com/questions/36836011/checking-validity-of-string-literal-union-type-at-runtime
+  for (const key in data.meaning) {
+    meanings.set(key as Language, data.meaning[key]);
+  }
+  return new GameEntry(
+    data.id,
+    data.writing,
+    data.reading_hiragana,
+    data.reading_romaji,
+    meanings,
+  );
+}
+function mapFromEntry(entry: GameEntry): any {
+  return copyFields2(entry, { meaning: mapToObject(entry.meaning) }, ['id']);
+}
+
+export const playerConverter: FConverter<PlayerInLobby> = {
+  toFirestore: (player: PlayerInLobby) =>
+    copyFields2(player, {
+      time_joined: FTimestamp.fromDate(player.time_joined),
+    }),
+  fromFirestore: (snapshot: FDocSnapshot) => {
+    const data = snapshot.data();
+    const ret = new PlayerInLobby(
+      data.uid,
+      data.name,
+      data.role,
+      data.status,
+      data.wins ?? 0,
+    );
+    ret.time_joined =
+      (data.time_joined as FTimestamp | null)?.toDate() ?? new Date();
+    return ret;
+  },
+};
+
+export const turnConverter: FConverter<GameTurn> = {
+  toFirestore: (turn: GameTurn) =>
+    copyFields2(
+      turn,
+      {
+        time_created: FTimestamp.fromDate(turn.time_created),
+        phase_start_time: FTimestamp.fromDate(turn.phase_start_time),
+        question: mapFromEntry(turn.question),
+        choices: turn.choices?.map(mapFromEntry),
+      },
+      ['id', 'player_responses'],
+    ),
+  fromFirestore: (snapshot: FDocSnapshot) => {
+    const data = snapshot.data();
+    const time_created =
+      (data.time_created as FTimestamp | null)?.toDate() ?? new Date();
+    const ret = new GameTurn(
+      snapshot.id,
+      data.ordinal,
+      time_created,
+      mapToEntry(data.question),
+      data.question_mode,
+      data.answer_mode,
+      data.choices?.map(mapToEntry),
+    );
+    ret.phase_start_time =
+      (data.phase_start_time as FTimestamp | null)?.toDate() ?? time_created;
+    ret.phase = data.phase || 'new';
+    return ret;
+  },
+};
+
+export const playerResponseConverter: FConverter<PlayerResponse> = {
+  toFirestore: (pdata: PlayerResponse) => copyFields(pdata),
+  fromFirestore: (snapshot: FDocSnapshot) => {
+    const data = snapshot.data();
+    const player_uid = snapshot.id;
+    return new PlayerResponse(
+      player_uid,
+      data.player_name,
+      data.answer_entry_id,
+      data.answer_typed,
+    );
+  },
+};
+
+export const userConverter: FConverter<QuizUser> = {
+  toFirestore: (user: QuizUser) => copyFields(user),
+  fromFirestore: (snapshot: FDocSnapshot) => {
+    const data = snapshot.data();
+    return new QuizUser(
+      data.uid,
+      data.email,
+      data.name,
+      data.is_admin ?? false,
+    );
+  },
+};
+
+export const userStatsConverter: FConverter<UserStats> = {
+  toFirestore: (user: UserStats) =>
+    copyFields2(
+      user,
+      { data: mapToObject(user.data, (val) => copyFields(val, ['entry_id'])) },
+      ['uid'],
+    ),
+  fromFirestore: (snapshot: FDocSnapshot) => {
+    const data = snapshot.data();
+    const statData = new Map<string, EntryStats>();
+    for (const key in data.data) {
+      const val = data.data[key];
+      statData.set(
+        key,
+        new EntryStats(
+          key,
+          val.reading_wins.val.reading_fails,
+          val.meaning_wins,
+          val.meaning_fails,
+          val.writing_wins,
+          val.writing_fails,
+        ),
+      );
+    }
+    const ret = new UserStats(snapshot.id);
+    ret.data = statData;
+    return ret;
+  },
+};
--- shared/src/types.ts
+++ shared/src/types.ts
@@ -6,6 +6,211 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
+export class GameLobby {
+  /** Null only during creation. Should be UTC time. */
+  time_created?: Date;
+
+  /* Must be fetched separately from a Firebase subcollection. */
+  players: Array<PlayerInLobby> = [];
+  /** The last "turn" is the current state of the game board.
+   * Must be fetched separately from a Firebase subcollection. */
+  turns: Array<GameTurn> = [];
+  /** ID of the most recent turn. */
+  current_turn_id?: string;
+
+  /** List of test groups selected for this lobby */
+  test_groups: Set<TestGroup> = new Set();
+  /** Questions remaining in the deck. */
+  questions: Array<GameEntry> = [];
+
+  /** ID of the next lobby, created as a copy of this lobby. */
+  next_lobby_id?: string;
+
+  constructor(
+    public id: string,
+    public creator_uid: string,
+    public settings: LobbySettings,
+    public status: LobbyStatus = 'new',
+  ) {}
+}
+
+export interface LobbySettings {
+  max_players: number;
+  question_mode: QuestionMode;
+  answer_mode: AnswerMode;
+  num_choices: number;
+  /** Answers are accepted for this number of seconds. */
+  turn_seconds: number;
+  /** If true, players can join after the game has started. */
+  allow_join_mid_game: boolean;
+  /** If true, card statistics will not be updated. */
+  freeze_stats: boolean;
+  /** Who is allowed to change lobby settings and kick players during the game. */
+  lobby_control: LobbyControl;
+}
+
+export function defaultLobbySettings(): LobbySettings {
+  return {
+    max_players: 20,
+    question_mode: 'kanji',
+    answer_mode: 'choose_romaji',
+    num_choices: 4,
+    turn_seconds: 5,
+    allow_join_mid_game: true,
+    freeze_stats: false,
+    lobby_control: 'players',
+  };
+}
+
+/** An entry that can be used as a question: a kanji, kana or word */
+export class GameEntry {
+  constructor(
+    /** Can be the same as writing */
+    public id: string,
+    /** For hiragana, this is the same as reading_hiragana. */
+    public writing: string,
+    public reading_hiragana: string,
+    public reading_romaji: string,
+    /** Maps language to a list of different meanings. */
+    public meaning: Map<Language, string[]>,
+  ) {}
+}
+
+export type QuestionMode = 'kanji' | 'hiragana' | 'romaji' | 'meaning';
+export type AnswerMode =
+  | 'choose_kanji'
+  | 'choose_hiragana'
+  | 'choose_romaji'
+  | 'choose_meaning'
+  | 'type_romaji'
+  | 'type_meaning'
+  | 'draw_kanji';
+/** Meaning is rendered in this language */
+export type Language = 'english'; // | 'russian';
+// "anyone" includes spectators
+export type LobbyControl = 'creator' | 'creator_or_czar' | 'players' | 'anyone';
+
+export type HiraganaGroup = 'hiragana' | 'hiragana_tuples';
+export type KatakanaGroup = 'katakana' | 'katakana_tuples';
+export type KanjiGrade =
+  | 'kanji_grade_1'
+  | 'kanji_grade_2'
+  | 'kanji_grade_3'
+  | 'kanji_grade_4'
+  | 'kanji_grade_5'
+  | 'kanji_grade_6'
+  | 'kanji_grade_s';
+export type KanjiJlptLevel =
+  | 'kanji_jlpt_1'
+  | 'kanji_jlpt_2'
+  | 'kanji_jlpt_3'
+  | 'kanji_jlpt_4'
+  | 'kanji_jlpt_5';
+/** Combines all possible groups. */
+export type TestGroup =
+  | HiraganaGroup
+  | KatakanaGroup
+  | KanjiGrade
+  | KanjiJlptLevel;
+
+/** Instance of a player specific to a single game lobby. */
+export class PlayerInLobby {
+  /** Should be UTC */
+  time_joined: Date = new Date();
+
+  constructor(
+    public uid: string,
+    public name: string,
+    public role: PlayerRole,
+    public status: PlayerStatus,
+    public wins: number,
+  ) {}
+}
+
+/** One turn, containing a single question. */
+export class GameTurn {
+  public phase: TurnPhase = 'new';
+  /** Time when the last phase bagan. */
+  phase_start_time: Date = this.time_created;
+
+  /** Maps player UID to what cards they played in this turn.
+   * Must be fetched separately from a Firebase subcollection.
+   * Making this a separate collection makes it secure for players to submit
+   * directly to Firestore, without a function.*/
+  player_responses: Map<string, PlayerResponse> = new Map();
+
+  constructor(
+    public id: string,
+    /** Turn's ordinal number: 1, 2, 3, ... */
+    public ordinal: number,
+    public time_created: Date,
+    public question: GameEntry,
+    public question_mode: QuestionMode,
+    public answer_mode: AnswerMode,
+    /** For 'typed' answer mode choices are empty. */
+    public choices?: GameEntry[],
+  ) {}
+}
+
+/** Player's submitted response. */
+export class PlayerResponse {
+  constructor(
+    public player_uid: string,
+    public player_name: string, // Copied from 'Players' for convenience.
+    /** Answer to a multiple-choice question. */
+    public answer_entry_id?: string,
+    public answer_typed?: string,
+  ) {}
+}
+
+export type PlayerRole = 'player' | 'spectator';
+
+export type PlayerStatus = 'online' | 'left' | 'banned';
+
+export type TurnPhase = 'new' | 'answering' | 'reveal' | 'complete';
+
+export type LobbyStatus = 'new' | 'in_progress' | 'ended';
+
+/**
+ * User data stored in the database.
+ * Users should only be referenced by their UIDs.
+ * Multiple users are allowed to have the same name!
+ */
+export class QuizUser {
+  constructor(
+    public uid: string,
+    public email: string | null | undefined = null,
+    public name: string,
+    public is_admin: boolean = false,
+  ) {}
+}
+
+/** User's stats, e.g. how many times they answered each question. */
+export class UserStats {
+  /** Maps entry ID to its stats for this user. */
+  public data: Map<string, EntryStats> = new Map();
+  constructor(public uid: string) {}
+}
+
+/** Stats for an individual entry, for an individual player. */
+export class EntryStats {
+  constructor(
+    public entry_id: string,
+    /** Stat for guessing reading from a kanji / kana / word,
+     * given a writing or meaning. */
+    public reading_wins?: number,
+    public reading_fails?: number,
+    /** Stat for guessing meaning from a kanji / word,
+     * given a writing or meaning. */
+    public meaning_wins?: number,
+    public meaning_fails?: number,
+    /** Stat for guessing writing of a kanji / kana / word,
+     * given a reading or meaning. */
+    public writing_wins?: number,
+    public writing_fails?: number,
+  ) {}
+}
+
 /** Used in Firebase RTDB to report user's online status */
 export type DBPresence = {
   state: 'online' | 'offline';
