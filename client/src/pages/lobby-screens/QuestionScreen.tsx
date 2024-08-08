f65fe47baeadbdd2d25da8460b2892a32013691d
Hunternif <hunternif@gmail.com>
2024-08-08 17:19:07 +0100

    Render question

--- client/src/pages/lobby-screens/QuestionScreen.tsx
+++ client/src/pages/lobby-screens/QuestionScreen.tsx
@@ -1,3 +1,42 @@
+import { CenteredLayout } from '../../components/layout/CenteredLayout';
+import { useGameContext } from './game-components/GameContext';
+
 export function QuestionScreen() {
-  return <div className="question">This is the Question screen</div>;
+  const { turn, isSpectator } = useGameContext();
+  return (
+    <CenteredLayout innerClassName="question-screen">
+      <QuestionCard text={turn.question.writing} />
+      <div className="choices">
+        {turn.choices?.map((c) => (
+          <Choice key={c.id} text={c.reading_romaji} readOnly={isSpectator} />
+        ))}
+      </div>
+    </CenteredLayout>
+  );
+}
+
+interface QuestionProps {
+  text: string;
+}
+function QuestionCard({ text }: QuestionProps) {
+  const classes = ['question-card'];
+  return (
+    <div className={classes.join(' ')}>
+      <span className="text">{text}</span>
+    </div>
+  );
+}
+
+interface ChoiceProps {
+  text: string;
+  selected?: boolean;
+  readOnly?: boolean;
+}
+
+function Choice({ text, selected, readOnly }: ChoiceProps) {
+  const classes = ['choice-card'];
+  if (selected) classes.push('selected');
+  if (readOnly) classes.push('readonly');
+  if (!readOnly) classes.push('hoverable-card');
+  return <div className={classes.join(' ')}>{text}</div>;
 }
